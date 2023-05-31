ca03967bc3f54aa380ba50a67576fa6432c74045
Sahil Salmanov <101056019+sahilsalmanov@users.noreply.github.com>
2023-05-31 12:11:39 +0400

    Delete src directory

--- src/store/store.js
+++
@@ -1,9 +0,0 @@
-
-import { configureStore } from "@reduxjs/toolkit";
-import taskReducer from "./tasksSlice";
-
-export default configureStore({
-    reducer:{
-        tasks: taskReducer,
-    }
-});
